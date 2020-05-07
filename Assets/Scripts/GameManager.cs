af9570629151ee82c1e862e3e4a76d99af95aaf0
Ricardo Moura <up201604912@fe.up.pt>
2020-05-07 16:20:42 +0100

    Add example scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public bool StartTimer { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            StartTimer = true;
+            StartCoroutine(HoldTimer());
+        }
+
+        if (Input.GetKeyUp(KeyCode.F1))
+        {
+            StartTimer = false;
+        }
+    }
+
+    IEnumerator HoldTimer()
+    {
+        Debug.Log("Starting Timer!");
+        yield return new WaitForSeconds(3f);
+        if (StartTimer)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
