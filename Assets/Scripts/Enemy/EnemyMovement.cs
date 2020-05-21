dc1e9bc5ab49558dfee6c68c882908cb3eac3a48
Ricardo Moura <up201604912@fe.up.pt>
2020-05-21 13:31:23 +0100

    New and improved Enemy (Prefab)

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -10,7 +10,7 @@ public class EnemyMovement : MonoBehaviour
     private Camera cam;
     private NavMeshAgent agent;
     private Rigidbody rigidBody;
-    private bool isVisible;
+    private Transform raycastTargets;
     public float offScreenDot = 0.8f;
     public 
     void Awake()
@@ -19,91 +19,44 @@ public class EnemyMovement : MonoBehaviour
         cam = Camera.main;
         agent = GetComponent<NavMeshAgent>();
         rigidBody = GetComponent<Rigidbody>();
+        raycastTargets = transform.Find("RaycastTargets");
     }
 
     void Update()
     {
-        Debug.Log(PlayerCanSeeMe());
         if (PlayerCanSeeMe())
         {
             agent.isStopped = true;
-            //rigidBody.isKinematic = true;
-            rigidBody.constraints = RigidbodyConstraints.FreezeAll;
-            //rigidBody.angularVelocity = Vector3.zero;
-            //Debug.Log("isKinematic");
-            //rigidBody.velocity = Vector3.zero;
+            rigidBody.isKinematic = true;
         } else
         {
-            //rigidBody.isKinematic = false;
-            //Debug.Log("NOT isKinematic");
-            rigidBody.constraints = RigidbodyConstraints.None;
+            rigidBody.isKinematic = false;
             agent.SetDestination(player.transform.position);
             agent.isStopped = false;
         }
     }
 
-    //se calhar e preciso mudar estas duas para o dot https://youtu.be/JAAGdF-Wdas
-    private void OnBecameVisible()
-    {
-        isVisible = true;
-    }
-
-    private void OnBecameInvisible()
-    {
-        isVisible = false;
-    }
 
     private bool PlayerCanSeeMe()
     {
-        return IsInView(player, gameObject);
-        if (!isVisible) return false;
-
         RaycastHit hit;
-        if (Physics.Raycast(cam.transform.TransformDirection(Vector3.forward), transform.position - cam.transform.position, out hit))
+
+        foreach(Transform raycastTarget in raycastTargets)
         {
-            Debug.Log(hit.collider.gameObject.name);
-            return hit.collider.CompareTag("Enemy");
+            Vector3 targetScreen = cam.WorldToViewportPoint(raycastTarget.position);
+            if (targetScreen.x < 0 || targetScreen.x > 1 || targetScreen.y < 0 || targetScreen.y > 1 || targetScreen.z <= 0)
+                continue;
+
+            //Debug.DrawLine(cam.transform.position, raycastTarget.position, Color.white);
+            if (Physics.Linecast(cam.transform.position, raycastTarget.position, out hit))
+            {
+                if (hit.transform.name == gameObject.name)
+                {
+                    return true;
+                }
+            }
         }
 
         return false;
-        /*Vector3 forward = Camera.main.transform.forward;
-        Vector3 other = (transform.position - playerTransform.position).normalized;
-        float dot = Vector3.Dot(forward, other);
-        Debug.Log(dot);
-        return dot > offScreenDot;*/
-    }
-
-    private bool IsInView(GameObject origin, GameObject toCheck)
-    {
-        Vector3 pointOnScreen = cam.WorldToScreenPoint(toCheck.GetComponentInChildren<Renderer>().bounds.center);
-
-        //Is in front
-        if (pointOnScreen.z < 0)
-        {
-            Debug.Log("Behind: " + toCheck.name);
-            return false;
-        }
-
-        //Is in FOV
-        if ((pointOnScreen.x < 0) || (pointOnScreen.x > Screen.width) ||
-                (pointOnScreen.y < 0) || (pointOnScreen.y > Screen.height))
-        {
-            Debug.Log("OutOfBounds: " + toCheck.name);
-            return false;
-        }
-
-        RaycastHit hit;
-        Vector3 heading = toCheck.transform.position - origin.transform.position;
-        Vector3 direction = heading.normalized;// / heading.magnitude;
-
-        if (Physics.Linecast(cam.transform.position, toCheck.GetComponentInChildren<Renderer>().bounds.center, out hit))
-        {
-            if (hit.transform.name != toCheck.name)
-            {
-                Debug.Log(toCheck.name + " occluded by " + hit.transform.name);
-                return false;
-            }
-        }
-        return true;
     }
 }
