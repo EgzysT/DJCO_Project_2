f2f47aae34953674285fad9227184319b7724aba
Pedro Xavier <pedro1998pinho@gmail.com>
2020-06-16 00:18:29 +0100

    Trying not to die

--- Assets/Scripts/WorldsChange/WorldChanger.cs
+++ Assets/Scripts/WorldsChange/WorldChanger.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 [RequireComponent(typeof(Shader))]
-public class WorldChanger : MonoBehaviour
-{
+public class WorldChanger : MonoBehaviour {
     public World belongsTo;
     public bool isParticleSystem;
     public bool isLight;
@@ -12,8 +12,7 @@ public class WorldChanger : MonoBehaviour
     private Pickable pickableObject;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         pickableObject = GetComponent<Pickable>();
 
         // Alter layer at the start
@@ -31,16 +30,14 @@ public class WorldChanger : MonoBehaviour
 
     void NormalWorldEnter(Vector3 effectOrigin) {
         float timeUntilEffect = (WorldsController.instance.maxRadius - Vector3.Distance(transform.position, effectOrigin)) / WorldsController.instance.effectSpeed;
-        if (gameObject.activeInHierarchy)
-        {
+        if (gameObject.activeInHierarchy) {
             StartCoroutine(HoldEffect(timeUntilEffect, true));
         }
     }
 
     void ArcaneWorldEnter(Vector3 effectOrigin) {
         float timeUntilEffect = Vector3.Distance(transform.position, effectOrigin) / WorldsController.instance.effectSpeed;
-        if (gameObject.activeInHierarchy)
-        {
+        if (gameObject.activeInHierarchy) {
             StartCoroutine(HoldEffect(timeUntilEffect, false));
         }
     }
@@ -79,21 +76,58 @@ public class WorldChanger : MonoBehaviour
     void CheckForObjectWorldChange(bool belongsToNormal) {
         // Will change worlds if it is a pickable object and the world changer is interacting with it
         if (pickableObject != null && pickableObject.isInteracting) {
-            Texture text = GetComponent<Renderer>().material.mainTexture;
 
-            // If it belongs to the Normal world then it will change to the Arcane world, and vice-versa
-            if (belongsToNormal) {
-                belongsTo = World.ARCANE;
-                GetComponent<Renderer>().material = new Material(Shader.Find("Custom/AppearShader"));
-            }
-            else {
-                belongsTo = World.NORMAL;
-                GetComponent<Renderer>().material = new Material(Shader.Find("Custom/DisappearShader"));
+            Renderer[] allChildren = GetComponentsInChildren<Renderer>();
+            foreach (Renderer childRenderer in allChildren) {
+                ChangeTexture(childRenderer, belongsToNormal);
             }
 
-            GetComponent<Renderer>().material.SetTexture("_MainTex", text);
+            ChangeTexture(GetComponent<Renderer>(), belongsToNormal);
+            //Texture textParent = GetComponent<Renderer>().material.mainTexture;
+
+            //// If it belongs to the Normal world then it will change to the Arcane world, and vice-versa
+            //if (belongsToNormal) {
+            //    belongsTo = World.ARCANE;
+            //    GetComponent<Renderer>().material = new Material(Shader.Find("Custom/AppearShader"));
+            //}
+            //else {
+            //    belongsTo = World.NORMAL;
+            //    GetComponent<Renderer>().material = new Material(Shader.Find("Custom/DisappearShader"));
+            //}
+
+            //GetComponent<Renderer>().material.SetTexture("_MainTex", textParent);
             gameObject.layer = LayerMask.NameToLayer("InteractiveWorld");
         }
+    }
+
+    private void ChangeTexture(Renderer rend, bool belongsToNormal) {
+        if (rend == null)
+            return;
+
+        Texture mainTexture = rend.material.GetTexture("_MainTex");
+        Texture normalMap = rend.material.GetTexture("_NormalMap");
+        Texture metallicMap = rend.material.GetTexture("_MetallicMap");
+        float metallic = rend.material.GetFloat("_Metallic");
+
+        Debug.Log(mainTexture);
+        Debug.Log(normalMap);
+        Debug.Log(metallicMap);
+        Debug.Log(metallic);
+
+        // If it belongs to the Normal world then it will change to the Arcane world, and vice-versa
+        if (belongsToNormal) {
+            belongsTo = World.ARCANE;
+            rend.material = new Material(Shader.Find("Custom/AppearShader"));
+        }
+        else {
+            belongsTo = World.NORMAL;
+            rend.material = new Material(Shader.Find("Custom/DisappearShader"));
+        }
+
+        rend.material.SetTexture("_MainTex", mainTexture);
+        rend.material.SetTexture("_NormalMap", normalMap);
+        rend.material.SetTexture("_MetallicMap", metallicMap);
+        rend.material.SetFloat("_Metallic", metallic);
     }
 
     void CheckParticleSystem(bool willAppear) {
@@ -109,8 +143,7 @@ public class WorldChanger : MonoBehaviour
         }
     }
 
-    void CheckLight(bool willAppear)
-    {
+    void CheckLight(bool willAppear) {
         if (!isLight)
             return;
 
